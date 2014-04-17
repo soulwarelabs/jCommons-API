566ea98cd2469d6a8d9d77db582896983cd0e448
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-04-17 12:41:38 +0400

    Box added

---
+++ src/main/java/com/soulwarelabs/jcommons/Box.java
@@ -0,0 +1,88 @@
+/*
+ * Project:  jCommons-API
+ * Outline:  jCommons framework API components
+ *
+ * File:     Box.java
+ * Folder:   /.../com/soulwarelabs/jcommons
+ * Revision: 1.02, 17 April 2014
+ * Created:  10 February 2014
+ * Author:   Ilya Gubarev
+ *
+ * Copyright (c) 2013-2014 Soulware Labs, Ltd.
+ * Contact information is available at http://www.soulwarelabs.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.soulwarelabs.jcommons;
+
+import java.io.Serializable;
+
+/**
+ * Value container.
+ *
+ * @since v1.0
+ *
+ * @author Ilya Gubarev
+ * @version 17 April 2014
+ */
+public class Box<T> implements Serializable {
+
+    private T value;
+
+    /**
+     * Creates a new instance of value container.
+     *
+     * @since v1.0
+     */
+    public Box() {
+
+    }
+
+    /**
+     * Creates a new instance of value container.
+     *
+     * @param value contained value.
+     *
+     * @since v1.0
+     */
+    public Box(T value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets a value contained inside.
+     *
+     * @return contained value.
+     *
+     * @since v1.0
+     */
+    public T getValue() {
+        return value;
+    }
+
+    /**
+     * Sets a new value to be contained inside.
+     *
+     * @param value contained value.
+     *
+     * @since v1.0
+     */
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", value);
+    }
+}
