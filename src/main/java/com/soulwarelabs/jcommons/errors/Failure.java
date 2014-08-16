1ba7ecec64128d50c0bb5d66500356173e2f45e1
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-08-17 02:14:58 +0400

    Failure data type package fixed

--- src/main/java/com/soulwarelabs/jcommons/errors/Failure.java
+++ src/main/java/com/soulwarelabs/jcommons/errors/Failure.java
@@ -23,7 +23,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.soulwarelabs.jcommons;
+package com.soulwarelabs.jcommons.errors;
 
 import java.io.Serializable;
 
