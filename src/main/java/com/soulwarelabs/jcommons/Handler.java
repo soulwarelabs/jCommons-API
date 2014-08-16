555e2cf2a09479daa087fe428d7804794e33eaa8
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-08-17 01:20:43 +0400

    Handler interface added

---
+++ src/main/java/com/soulwarelabs/jcommons/Handler.java
@@ -0,0 +1,53 @@
+/*
+ * Project:  jCommons-API
+ * Outline:  jCommons framework API components
+ *
+ * File:     Handler.java
+ * Folder:   /.../com/soulwarelabs/jcommons
+ * Revision: 1.01, 17 August 2014
+ * Created:  17 August 2014
+ * Authors:  Ilya Gubarev
+ *
+ * Copyright (c) 2013-2014 Soulware Labs, Ltd.
+ * Contact information is available at http://www.soulwarelabs.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.soulwarelabs.jcommons;
+
+/**
+ * Input data processor.
+ * <p>
+ * Designed to be used for implementing callbacks.
+ *
+ * @param <R> result type.
+ * @param <T> input data type.
+ *
+ * @since v1.1.0
+ *
+ * @author Ilya Gubarev
+ * @version 17 August 2014
+ */
+public interface Handler<R, T> {
+
+    /**
+     * Performs input data processing.
+     *
+     * @param data input data to be processed.
+     * @return processing result.
+     * @throws RuntimeException if error occures while processing data.
+     *
+     * @since v1.1.0
+     */
+    R call(T data);
+}
