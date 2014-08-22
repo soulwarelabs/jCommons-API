6b51715235588882b284cf39a8709a9df6e9605c
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-08-22 16:41:04 +0400

    Version added

---
+++ src/main/java/com/soulwarelabs/jcommons/data/Version.java
@@ -0,0 +1,37 @@
+/*
+ * Project:  jCommons-API
+ * Outline:  jCommons framework API components
+ *
+ * File:     Version.java
+ * Folder:   /.../com/soulwarelabs/jcommons/data
+ * Revision: 1.04, 17 August 2014
+ * Created:  10 May 2014
+ * Authors:  Ilya Gubarev
+ *
+ * Copyright (c) 2013-2014 Soulware Labs, Ltd.
+ * Contact information is available at http://www.soulwarelabs.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.soulwarelabs.jcommons.data;
+
+import java.io.Serializable;
+
+public class Version implements Serializable {
+
+    // NOTE: version can look like 42-alpha.073-beta.0-gamma...-SNAPHOT
+
+    public Version() {
+
+    }
+}
