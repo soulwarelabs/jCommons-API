c51f6c2fece8d10fa7c04d6385c8e697f2c24b98
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-08-23 15:59:04 +0400

    HiddenField and SecretField javadocs updated

--- src/main/java/com/soulwarelabs/jcommons/namings/HiddenField.java
+++ src/main/java/com/soulwarelabs/jcommons/namings/HiddenField.java
@@ -1,3 +1,28 @@
+/*
+ * Project:  jCommons-API
+ * Outline:  jCommons framework API components
+ *
+ * File:     HiddenField.java
+ * Folder:   /.../com/soulwarelabs/jcommons/namings
+ * Revision: 1.01, 23 August 2014
+ * Created:  17 August 2014
+ * Authors:  Ilya Gubarev
+ *
+ * Copyright (c) 2013-2014 Soulware Labs, Ltd.
+ * Contact information is available at "http://www.soulwarelabs.com".
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       "http://www.apache.org/licenses/LICENSE-2.0".
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.soulwarelabs.jcommons.namings;
 
 import java.lang.annotation.ElementType;
@@ -5,6 +30,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Hidden data structure field.
+ *
+ * @since v1.1.0
+ *
+ * @author Ilya Gubarev
+ * @version 23 August 2014
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface HiddenField {
