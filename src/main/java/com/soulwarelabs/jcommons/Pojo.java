d0da4ebb6e36f98cff6379c5f0ed130eb7bc09b7
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-08-23 14:57:46 +0400

    Pojo API updated: not Printable now

--- src/main/java/com/soulwarelabs/jcommons/Pojo.java
+++ src/main/java/com/soulwarelabs/jcommons/Pojo.java
@@ -2,7 +2,7 @@ package com.soulwarelabs.jcommons;
 
 import java.io.Serializable;
 
-public abstract class Pojo implements Copyable, Printable , Serializable {
+public abstract class Pojo implements Copyable, Serializable {
 
     public static String print(Object object) {
         throw new UnsupportedOperationException();
