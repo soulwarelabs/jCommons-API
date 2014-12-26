f55a308b87a295c972600f9e76ab864bc74195da
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-12-27 00:07:19 +0300

    User updated

--- src/main/java/com/soulwarelabs/jcommons/api/security/User.java
+++ src/main/java/com/soulwarelabs/jcommons/api/security/User.java
@@ -1,5 +1,58 @@
+/*
+ * Project:  jCommons API
+ * Outline:  jCommons framework API components
+ *
+ * File:     User.java
+ * Folder:   src/main/java/com/soulwarelabs/jcommons/api/security
+ * Revision: 1.01, 06 December 2014
+ * Created:  17 August 2014
+ * Authors:  Ilya Gubarev
+ *
+ * Copyright (c) 2014 Soulware Labs, Ltd.
+ * Contact information is available at "http://www.soulwarelabs.com".
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       "http://www.apache.org/licenses/LICENSE-2.0".
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.soulwarelabs.jcommons.api.security;
 
-public class User {
+import com.soulwarelabs.jcommons.api.Unique;
 
+public class User extends Unique<Integer> {
+
+    private String login;
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(Integer id, String login, String name) {
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
